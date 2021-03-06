7a353171c428cb3a92adb874107e77b44e8da7de
GellertPalfi <palfigell9@gmail.com>
2021-03-06 14:23:41 +0100

    updated requirements.txt, moved urls to seperate file

--- buy_sell/api_getter.py
+++ buy_sell/api_getter.py
@@ -1,19 +1,16 @@
 import requests
 import json
+from buy_sell.buy_sell_urls import *
 
 
 def get_okex_data():
-    okex_data = requests.get('https://www.okex.com/api/spot/v3/instruments/ETH-USDT/book')
+    okex_data = requests.get(OKEX_ETH_DATA)
     okex_data_dict = json.loads(okex_data.text)
     return okex_data_dict
 
 
 def get_binance_data():
-    params = {
-        'symbol': 'ETHUSDT',
-        # 'limit': 100, documentation says default is 500 but in reality its 100
-    }
-    binance_data = requests.get('https://api.binance.com/api/v3/depth', params=params)
+    binance_data = requests.get(BINANCE_ETH_DATA, params=BINANCE_ETH_SYMBOL_DICT)
     binance_data_dict = json.loads(binance_data.text)
     return binance_data_dict
 
---
+++ buy_sell/buy_sell_urls.py
@@ -0,0 +1,6 @@
+
+OKEX_ETH_DATA = 'https://www.okex.com/api/spot/v3/instruments/ETH-USDT/book'
+BINANCE_ETH_DATA = 'https://api.binance.com/api/v3/depth'
+BINANCE_ETH_SYMBOL_DICT = {
+        'symbol': 'ETHUSDT',
+    }
